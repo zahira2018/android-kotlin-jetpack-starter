d6ccf223cdf9b24c48450af03f75dee777064986
Zahira Salsabila <ikmal1993@gmail.com>
2019-09-26 16:16:57 +0700

    [md-001] Adding navigation component

--- app/src/main/java/com/iproject/androidkotlinjetpackstarter/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/iproject/androidkotlinjetpackstarter/viewmodel/UserViewModel.kt
@@ -2,9 +2,29 @@ package com.iproject.androidkotlinjetpackstarter.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.iproject.androidkotlinjetpackstarter.model.view.GithubUserView
+import com.iproject.androidkotlinjetpackstarter.model.database.GithubUser
 
 class UserViewModel : ViewModel() {
 
-    val githubUserViewList = MutableLiveData<List<GithubUserView>>()
+    private val githubUserViewList = MutableLiveData<List<GithubUser>>()
+    private val userListLoading = MutableLiveData<Boolean>()
+    private val userListError = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        val user1 = GithubUser(
+            "Ronaldo",
+            "1",
+            "https://cdn.images.express.co.uk/img/dynamic/67/590x/Why-is-Cristiano-Ronaldo-not-playing-for-Juventus-vs-Brescia-after-Fifa-Best-awards-snub-1182201.jpg?r=1569356281389"
+        )
+        val user2 = GithubUser(
+            "Messi",
+            "2",
+            "https://www.fcbarcelona.com/photo-resources/2019/05/13/cb6216c0-1086-4ca1-bcc3-9808deb61fd4/mini_Messi-celebraci-gol.jpg?width=1200&height=750"
+        )
+        val userList = listOf(user1, user2)
+
+        githubUserViewList.value = userList
+        userListLoading.value = false
+        userListError.value = false
+    }
 }
