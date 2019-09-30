bc8fda5b005a1d5fa9d56ae380990b608a894205
Zahira Salsabila <ikmal1993@gmail.com>
2019-09-30 18:28:56 +0700

    [md-002] Implementing list and detail list using hardcode data

--- app/src/main/java/com/iproject/androidkotlinjetpackstarter/view/DetailFragment.kt
+++ app/src/main/java/com/iproject/androidkotlinjetpackstarter/view/DetailFragment.kt
@@ -6,13 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 
 import com.iproject.androidkotlinjetpackstarter.R
+import com.iproject.androidkotlinjetpackstarter.viewmodel.DetailViewModel
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
+    private lateinit var viewModel: DetailViewModel
     private var userUUID = 0
 
     override fun onCreateView(
@@ -25,9 +29,21 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
         arguments?.let {
             userUUID = DetailFragmentArgs.fromBundle(it).userUUID
+            viewModel.fetch(userUUID.toString())
         }
+
+        observerViewModel()
+    }
+
+    private fun observerViewModel() {
+        viewModel.userLiveData.observe(this, Observer { user ->
+            user?.let {
+                detail_name.text = it.login
+            }
+
+        })
     }
 }
--- app/src/main/java/com/iproject/androidkotlinjetpackstarter/view/ListFragment.kt
+++ app/src/main/java/com/iproject/androidkotlinjetpackstarter/view/ListFragment.kt
@@ -6,12 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.iproject.androidkotlinjetpackstarter.R
+import com.iproject.androidkotlinjetpackstarter.view.adapter.UserListAdapter
+import com.iproject.androidkotlinjetpackstarter.viewmodel.UserViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: UserViewModel
+    private val userListAdapter = UserListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +32,44 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(UserViewModel::class.java)
+        viewModel.refresh()
 
+        list_recycler.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userListAdapter
+        }
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.githubUserList.observe(this, Observer { users ->
+            users?.let {
+                list_recycler.visibility = View.VISIBLE
+                userListAdapter.updateUserList(it)
+            }
+
+        })
+
+        viewModel.userListError.observe(this, Observer { isError ->
+            isError?.let {
+                list_error.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.userListLoading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                if (it) {
+                    list_progress_bar.visibility = View.VISIBLE
+                    list_text_progress.visibility = View.VISIBLE
+                    list_recycler.visibility = View.GONE
+                    list_error.visibility = View.GONE
+                } else {
+                    list_progress_bar.visibility = View.GONE
+                    list_text_progress.visibility = View.GONE
+                }
+            }
+        })
     }
 }
