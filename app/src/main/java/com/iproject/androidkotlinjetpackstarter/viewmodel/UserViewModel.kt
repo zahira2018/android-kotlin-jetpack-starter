6a5a0d68649ec320d6b815ef6751692338e09f7e
Zahira Salsabila <ikmal1993@gmail.com>
2019-10-01 16:59:27 +0700

    [md-003] Delete content hardcode in listviewmodel

--- app/src/main/java/com/iproject/androidkotlinjetpackstarter/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/iproject/androidkotlinjetpackstarter/viewmodel/UserViewModel.kt
@@ -11,22 +11,6 @@ class UserViewModel : ViewModel() {
     val userListError = MutableLiveData<Boolean>()
 
     fun refresh() {
-        val user1 = GithubUser(
-            "Ronaldo",
-            "1",
-            "https://cdn.images.express.co.uk/img/dynamic/67/590x/Why-is-Cristiano-Ronaldo-not-playing-for-Juventus-vs-Brescia-after-Fifa-Best-awards-snub-1182201.jpg?r=1569356281389"
-        )
-        val user2 = GithubUser(
-            "Messi",
-            "2",
-            "https://www.fcbarcelona.com/photo-resources/2019/05/13/cb6216c0-1086-4ca1-bcc3-9808deb61fd4/mini_Messi-celebraci-gol.jpg?width=1200&height=750"
-        )
-        val userList = arrayListOf(user1, user2)
-
-        githubUserList.value = userList
-        userListLoading.value = false
-        userListError.value = false
-
-
+        
     }
 }
