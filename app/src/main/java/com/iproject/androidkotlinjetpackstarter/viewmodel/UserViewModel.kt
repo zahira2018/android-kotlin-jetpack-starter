8dc45af78bc1a0a7a77ee65ff41750a0c6092552
Zahira Salsabila <ikmal1993@gmail.com>
2019-10-02 16:19:17 +0700

    [md-003] create baseviewmodel and modifying userviewmodel

--- app/src/main/java/com/iproject/androidkotlinjetpackstarter/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/iproject/androidkotlinjetpackstarter/viewmodel/UserViewModel.kt
@@ -1,17 +1,18 @@
 package com.iproject.androidkotlinjetpackstarter.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.iproject.androidkotlinjetpackstarter.api.UsersApiService
+import com.iproject.androidkotlinjetpackstarter.model.database.DatabaseHelper
 import com.iproject.androidkotlinjetpackstarter.model.database.GithubUser
 import com.iproject.androidkotlinjetpackstarter.model.view.GithubUserView
-import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class UserViewModel : ViewModel() {
+class UserViewModel(application: Application) : BaseViewModel(application) {
 
     private val userService = UsersApiService()
     private val disposable = CompositeDisposable()
@@ -42,9 +43,16 @@ class UserViewModel : ViewModel() {
                 }
                 .subscribeWith(object : DisposableSingleObserver<List<GithubUserView>>() {
                     override fun onSuccess(t: List<GithubUserView>) {
-                        githubUserViewList.value = t
-                        userListError.value = false
-                        userListLoading.value = false
+                        val githubUserList = t.map {
+                            GithubUser(
+                                login = it.login,
+                                id = it.id,
+                                avatarUrl = it.avatarUrl
+                            )
+                        }
+                        storeUsersLocally(githubUserList)
+
+
                     }
 
                     override fun onError(e: Throwable) {
@@ -54,6 +62,33 @@ class UserViewModel : ViewModel() {
 
                 })
         )
+    }
+
+    private fun usersRetrieved(list: List<GithubUserView>) {
+        githubUserViewList.value = list
+        userListError.value = false
+        userListLoading.value = false
+    }
+
+    private fun storeUsersLocally(list: List<GithubUser>) {
+        launch {
+            val dao = DatabaseHelper(getApplication()).githubUserDao()
+            dao.deleteAllUsers()
+            val result = dao.insertAll(*list.toTypedArray())
+            var i = 0
+            while (i < list.size) {
+                list[i].uuid = result[i].toInt()
+                ++i
+            }
+            val usersView = list.map {
+                GithubUserView(
+                    login = it.login,
+                    id = it.id.toString(),
+                    avatarUrl = it.avatarUrl
+                )
+            }
+            usersRetrieved(usersView)
+        }
     }
 
     override fun onCleared() {
